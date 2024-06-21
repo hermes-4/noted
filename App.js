65d1cccbc2cb7ac0382edd4023746b600b9a9fbf
hermes-4 <ebenezeroforia08@gmail.com>
2024-06-21 10:12:42 +0000

    Second Commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Create from './pages/CreateNew';
+
+export default function App() {
+  return (
+    <SafeAreaView 
+    // style={styles.container}
+    >
+      <Create/>
+      <StatusBar style="auto" />
+      </SafeAreaView> 
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
